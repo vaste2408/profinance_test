c23a39ce38ae47062843648af6f96bdaa8043211
vaste2408 <ois.vasiljev.aa@mail.ru>
2022-04-05 09:56:48 +1000

    Base project

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const DOM_FORM = document.getElementById('mainForm');
+    const DOM_BTN_SUBMIT = document.getElementById('submitMainForm');
+    const DOM_INPUT = document.getElementById('url');
+    const DOM_ANSWER = document.getElementById('serverAnswer');
+
+
+    //e.classList.add('last');
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>PROFINANCE TEST</title>
+
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+</head>
+<body class="antialiased">
+<form id="mainForm">
+    <label> Input url
+        <input name="url" type="url" id="url" />
+    </label>
+    <button id="submitMainForm">Submit</button>
+</form>
+<div id="serverAnswer"></div>
+</body>
+<script src="{{ mix('/js/app.js') }}"></script>
+</html>
